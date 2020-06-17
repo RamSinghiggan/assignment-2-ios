8586e133bc13b8fe885882c84144c9f7133e93b8
RamSinghIggan <57167406+RamSinghiggan@users.noreply.github.com>
2020-06-16 20:30:28 -0400

    Initial Commit

---
+++ toVisitPlaces_ram_c0779370/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  toVisitPlaces_ram_c0779370
+//
+//  Created by rschakar on 6/16/20.
+//  Copyright © 2020 rs_chakar. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
